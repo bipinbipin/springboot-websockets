ef233332e5ec7cc61a61830326ff374747561e4b
bipin <bipinbipin@gmail.com>
2017-04-24 23:55:08 -0500

    Initial commit

---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,21 @@
+package hello;
+
+/**
+ * Created by bipin on 4/24/17.
+ */
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
